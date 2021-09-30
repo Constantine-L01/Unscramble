40f25fda837645e65b1f3a0f180e5766074c7677
Constantine-L01 <100073484@student.swin.edu.au>
2021-09-30 22:39:33 +0800

    Store data in ViewModel 	Finished Changes to be committed: 	modified:   app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt 	modified:   app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -40,6 +40,16 @@ class GameViewModel : ViewModel() {
     }
 
     /*
+    * Re-initializes the game data to restart the game.
+    */
+    fun reinitializeData() {
+        _score = 0
+        _currentWordCount = 0
+        wordsList.clear()
+        getNextWord()
+    }
+
+    /*
     * Returns true if the current word count is less than MAX_NO_OF_WORDS.
     * Updates the next word.
     */
